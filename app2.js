7d8876c2974c81a5bd4297e28e9ee6f943a34240
星火朱伟 <zhuwei@126.com>
2019-06-11 16:40:07 +0800

    first commit

---
+++ app2.js
@@ -0,0 +1,25 @@
+// npm i koa koa-static koa-gzip koa-cors koa-etag path  chalk koa-router -D
+
+
+var koa = require("koa")
+var serve = require("koa-static")
+var path = require("path")
+var PORT = 3002
+var HOST = "localhost"
+
+var app = new koa();
+
+app
+
+    .use(serve(path.join(__dirname, '/app2')))
+
+    .listen(PORT, function () {
+
+        console.log(
+            '\n' +
+            ' OPEN '+
+            ' Server listening on ' +
+            `\nhttp://${HOST}:${PORT}\n`
+        )
+    })
+
